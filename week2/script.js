11c8e8001e86417a220c154cf3352ff6a424763d
raquellchavezz <raquel.chavez41313@gmail.com>
2023-01-26 19:51:54 -0800

    adds checkbox element and and assigns attributes to checkbox

--- week2/script.js
+++ week2/script.js
@@ -1,6 +1,15 @@
+// adding new ingredient (whip cream) to page: 
+
 // create new list item & store in a variable 
 let newListItem = document.createElement('li');
 //update the text content of that last item 
 newListItem.textContent = 'non-dairy whip cream';
 //add that list item as a child of the ul 
 document.querySelector('ul').appendChild(newListItem); 
+
+//adding checkmark to element to a list: 
+
+//creating a check mark as an element to a list
+const myDiv = document.getElementById(myDiv);
+//creating checkbox element 
+let checkbox = document. createElement("input"); 
