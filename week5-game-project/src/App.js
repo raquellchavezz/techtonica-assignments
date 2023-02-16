bba93b590c308f571d875d7cbce4f4b8406db461
raquellchavezz <raquel.chavez41313@gmail.com>
2023-02-15 21:25:17 -0800

    Remove node_modules

--- week5-game-project/src/App.js
+++ week5-game-project/src/App.js
@@ -4,18 +4,22 @@
 //  should say `"Hi yourFirstName yourLastName"`
 
 import "./styles.css";
-import Dice from "./components/Dice.js";
+import Dice from "./components/Dice.js"; //in dice.js making the component but wont be used until imported and initialized
 export default function App() {
   const player = "Player 1 ";
   const image = "../images/dice1.png";
   return (
     <div className="App">
-      <Dice player={player} image={image} />
+      <Dice player={player} image={image} />  {/* initializing- putting the component on the page*/}
       <Dice player={"player 2"} image={image} />
     </div>
   );
 }
 
+//canput both imgs in dice component- may be confusing want to keep components simple
+//could seperate so the button is just in app.js so controlled thru there & have new component thats just these images 
+//dice component that just shows img- tell what img to show and shows it 
+// app will have button + func to roll 
 // we imported the component
 // we are doing this step so its imported into your diff files so it can
 // hi.js is where we are making reusable code
