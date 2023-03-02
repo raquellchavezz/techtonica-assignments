c616d4b9a14c0b8c51c73863c7cad911d52ce740
raquellchavezz <raquel.chavez41313@gmail.com>
2023-03-01 22:19:08 -0800

    api is not fetching city, working on that now

--- week7/client/src/App.js
+++ week7/client/src/App.js
@@ -4,29 +4,34 @@ import WeatherForm from './components/weatherForm';
 import WeatherCard from './components/weatherCard';
 
 function App() {
-  const [city, setCity] = useState("");
-  const [result, setResult] = useState(null);
+  const [city, setCity] = useState(""); //need to keep track of city
+  const [result, setResult] = useState(null); //what api sends us will be result
 
   //A function to do the get request and set the state from the hard code data
-  const loadCity = () => {
-    fetch("http://localhost:8080/api/weather")
-      .then((response) => response.json())
+  const loadCity = () => { //will get called later below
+    fetch(`http://localhost:8082/api/weather/${city}`) //not gettign back a city, look at api for weather api and wah do i have to send it so i know what city I want 
+    //param is city which is the user input   --> its getting this from your backend
+    //how do we get value from the form? 
+    //something to manage (onChange) the data 
+    //what do we do when someone submits the form 
+    .then((response) => response.json())
       .then((result) => {
-        //console.log(result)
-        setCity(result.weather[0].name);
-        setResult(result);
+        console.log(result)
+        // setCity(result.weather[0].name);
+        // setResult(result);
       });
   }
 
  const handleSubmit = (e) =>{
   e.preventDefault();
-  loadCity();
+  loadCity(); //calling load ciity, in load city call whatever path in load city
  }
 
 
   return (
     <div className="App">
-      <WeatherForm city={city} handleSubmit={handleSubmit}/>
+      {/* now we need to connect to the weather form */}
+      <WeatherForm setCity={setCity} handleSubmit={handleSubmit}/>
       {!result ? <p>Please click the botton to see Data</p> : <WeatherCard data={result} /> }
     </div>
   );
--- week7/server/server.js
+++ week7/server/server.js
@@ -5,7 +5,7 @@ const dataWeather = require("./data")
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 8082;
 app.use(cors());
 app.use(express.json());
 
@@ -13,10 +13,33 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from My template ExpressJS' });
   });
-
+//app.get calling get function & we are passing string /api/weather which is the route/url
+//the thing that happens (req,res) is itself a function which is a callback 
+// req & res are kind of returned from the 
+//app.get takes a path in a callback function 
+//callback lets us use req and res variables to do stuff
 // creates an endpoint for the route /api/weather
-app.get('/api/weather', (req, res) => {
-  res.json(dataWeather);
+app.get('/api/weather', (req, res) => { // req = request, res = response --> its basically a callback listen for request matches specifc routes
+  const city = req.query.CityName;  // for city we are setting the variable to the requested query which the user will input (they will choose a city)
+  const apiKey = process.env.API_KEY; //environment variable set in .env file
+  const params = new URLSearchParams({ //creating the params variable and setting it to whatevr URLSearchParams which is a built in express thing     q: req.query.City.Name, // the query that we will be searching for
+   //a way to create a diff kind of query string without having to re-type everything
+    appid: process.env.API_KEY, //my api key to make requests to the database
+    units: "imperial", //converting to fahrenheit
+    city: city //set to city --> req.query.CityName
+  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;  //in backend making req
+  console.log(url)
+  fetch(url)
+  .then((response) => response.json()) //response sends back saying ok weather api give me weather for x city, response will be weather for x city need as json so browser can understand 
+  .then((result) => {
+    console.log("Success:", result);
+    res.send(result); //result variable needs to be inside scope bc variable won't exist outside of scope
+  }) 
+  .catch((error) => {
+    console.error("Error:", error);
+  }); //if have error console this error
+  //result = response in json format
 });
 
 // console.log that your server is up and running
